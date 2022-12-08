0ec4d8dc16124f15d83059f40525df4357c738e0
jacsonikowy <michal6345@wp.pl>
2022-12-08 21:10:47 +0100

    Changed backend of SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,17 @@
 const SearchBar = () => {
 
-  const handleClick = (searchValue) => {
-    window.location.replace(`https://www.google.com/search?q=${searchValue}`);
-  }
-
-  const handleKeyPress = (searchValue) => {
-    window.location.replace(`https://www.google.com/search?q=${searchValue}`);
+  const handleSearch = (searchValue) => {
+    window.location.href = `https://www.google.com/search?q=${searchValue}`;
   }
   
   return (
     <div className={"flex justify-center h-20"}>
-      <input className={"border-white bg-inherit bg-neutral-900 rounded-3xl w-2/3 h-2/3 text-xl p-5"} type="text" id="input" placeholder="Type anything you want" onKeyPress={(searchValue) => handleKeyPress(document.querySelector("#input").value)}/>
-      <button onClick={() => handleClick(document.querySelector("#input").value)}>Search</button>
+      <input className={"border-white bg-inherit bg-neutral-900 rounded-3xl w-2/3 h-2/3 text-xl p-5"} type="text" id="input" placeholder="Type anything you want" onKeyPress={(event) => {
+        if (event.key === 'Enter') {
+          handleSearch(document.querySelector("#input").value);
+        }
+      }} />
+      <button onClick={() => handleSearch(document.querySelector("#input").value)}>Search</button>
     </div>
   ) 
 }
